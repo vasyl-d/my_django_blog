ca132ca20b8517d3f4f470b8251ba7c2a6c7d522
vasyl-d <vpets07@gmail.com>
2022-12-07 17:26:31 +0200

    pagination

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.views import View
+from django.core.paginator import Paginator
+from .models import Post
+
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            'myblog/home.html'
-        )
+        posts = Post.objects.all() 
+        paginator = Paginator(posts, 3) # 3 объекта на странице
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'myblog/home.html', context={
+            'page_obj': page_obj
+        })
