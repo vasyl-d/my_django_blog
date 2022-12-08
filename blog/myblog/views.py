6fe37a08343b05ec398a1c1cecb003f1fcd5c0e1
vasyl-d <vpets07@gmail.com>
2022-12-08 17:41:54 +0200

    added post detail view

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
@@ -15,3 +15,10 @@ class MainView(View):
         return render(request, 'myblog/home.html', context={
             'page_obj': page_obj
         })
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'myblog/post_detail.html', context={
+            'post': post
+    })
