e22cf41e54c464afb2ef00b519953122786e2089
vasyl-d <vpets07@gmail.com>
2022-12-23 11:38:05 +0200

    added search

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -6,6 +6,7 @@ from .forms import SigUpForm, SignInForm, FeedBackForm
 from django.contrib.auth import login
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.mail import send_mail, BadHeaderError
+from django.db.models import Q
 
 
 class MainView(View):
@@ -92,4 +93,21 @@ class SuccessView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'myblog/success.html', context={
             'title': 'Спасибо'
+        })
+
+class SearchResultsView(View):
+    def get(self, request, *args, **kwargs):
+        query = self.request.GET.get('q')
+        results = ""
+        if query:
+            results = Post.objects.filter(
+                Q(h1__icontains=query) | Q(content__icontains=query)
+            )
+        paginator = Paginator(results, 6)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'myblog/search.html', context={
+            'title': 'Поиск',
+            'results': page_obj,
+            'count': paginator.count
         })
