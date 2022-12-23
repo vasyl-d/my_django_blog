88f951237df5482610a51ebc4dcec6409cef6494
vasyl-d <vpets07@gmail.com>
2022-12-23 16:02:37 +0200

    added tag model, tag view

--- blog/myblog/admin.py
+++ blog/myblog/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, FeedBack
+from .models import Post, FeedBack, Tag
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'url': ('title',)}
 
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'url': ('name',)}
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(FeedBack)
+admin.site.register(Tag, TagAdmin)
